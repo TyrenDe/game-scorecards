e0c79b8b67d8d7d15b47d96aa1e64f91efcb9c4d
Shane DeSeranno <tyren@syntheticstream.com>
2018-11-10 10:15:44 -0800

    Updates

---
+++ precache-manifest.703e6b5ce40ccdaa6ebf68bda5cc7cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game-scorecards/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d076c119b960ad65f287",
+    "url": "/game-scorecards/static/js/runtime~main.d076c119.js"
+  },
+  {
+    "revision": "69c3c352583929cdbd49",
+    "url": "/game-scorecards/static/js/main.69c3c352.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/game-scorecards/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "69c3c352583929cdbd49",
+    "url": "/game-scorecards/static/css/main.d4218ee2.chunk.css"
+  },
+  {
+    "revision": "e54e62cb04ffdbc98896fe3731697e4b",
+    "url": "/game-scorecards/index.html"
+  }
+];
