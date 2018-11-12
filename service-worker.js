93d50e51b4f698054f8ca2e4fdee3c7e28c299e5
Shane DeSeranno <tyren@syntheticstream.com>
2018-11-11 20:04:29 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/game-scorecards/precache-manifest.7e759f2a8d955868f8a5b832ddc53cb3.js"
+  "/game-scorecards/precache-manifest.ae0296f5e831f1053bd8804baa960d2a.js"
 );
 
 workbox.clientsClaim();
